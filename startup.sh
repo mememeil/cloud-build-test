8bba93914406cc0c253112a428d05ca0af3804fd
mememeil <kodhikodhi@gmail.com>
2020-09-26 16:52:56 +0900

    スクリプトを改修

--- startup.sh
+++ startup.sh
@@ -4,6 +4,6 @@ su kodhikodhi -
 
 cd ~/
 
-git clone https://github.com/mememeil/cloud-build-test.git .
+git clone https://github.com/mememeil/cloud-build-test.git cloud-build-test
 
 inspec exec cloud-build-test/inspec/controls/test-server1.rb -t ssh://kodhikodhi@10.138.0.9 > /home/kodhikodhi/result1
