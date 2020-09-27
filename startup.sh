34a3b4430525612d7561d2dbafae11b461d14502
mememeil <kodhikodhi@gmail.com>
2020-09-27 16:39:58 +0900

    cloudbuild.yaml ip@変更

--- startup.sh
+++ startup.sh
@@ -3,4 +3,4 @@
 eval $(ssh-agent)
 ssh-add /root/.ssh/id-rsa
 git clone https://github.com/mememeil/cloud-build-test.git /root/cloud-build-test
-inspec exec cloud-build-test/inspec/controls/test-server1.rb -t ssh://kodhikodhi@10.138.0.10 > /home/kodhikodhi/result 2>&1
+inspec exec cloud-build-test/inspec/controls/test-server1.rb -t ssh://kodhikodhi@10.138.0.11 > /home/kodhikodhi/result 2>&1
