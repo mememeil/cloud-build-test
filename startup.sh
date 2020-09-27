50ec369eab65e3f8bffc5a28d99f0d3b8286bc80
mememeil <kodhikodhi@gmail.com>
2020-09-27 19:30:34 +0900

    スクリプト結果ファイルのディレクトリ変更

--- startup.sh
+++ startup.sh
@@ -4,11 +4,8 @@ cat <<EOF > /root/starup.sh
 eval $(ssh-agent)
 ssh-add /root/.ssh/id_rsa
 git clone https://github.com/mememeil/cloud-build-test.git /root/cloud-build-test
-inspec exec cloud-build-test/inspec/controls/test-server1.rb -t ssh://kodhikodhi@10.138.0.11 > /home/kodhikodhi/result 2>&1
+touch /root/result
+inspec exec cloud-build-test/inspec/controls/test-server1.rb -t ssh://kodhikodhi@10.138.0.11 > /root/result 2>&1
 EOF
 
-sleep 10s
-source /root/starup.sh
-
-sleep 10s
 source /root/starup.sh
