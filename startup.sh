5a6f502f4f6ae02e5fc5dc2da631772cfae46d2e
mememeil <kodhikodhi@gmail.com>
2020-09-27 15:58:36 +0900

    inspecサーバーイメージ変更。秘密鍵追加

--- startup.sh
+++ startup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-su - kodhikodhi <<EOF
 git clone https://github.com/mememeil/cloud-build-test.git cloud-build-test
 eval $(ssh-agent)
-ssh-add
+ssh-add /root/.ssh/id-rsa
 inspec exec cloud-build-test/inspec/controls/test-server1.rb -t ssh://kodhikodhi@10.138.0.10 > /home/kodhikodhi/result 2>&1
-EOF
