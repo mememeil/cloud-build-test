4573caba3c8041a07a222a8502dd7f56eeb7e5b0
mememeil <kodhikodhi@gmail.com>
2020-09-27 17:20:48 +0900

    スタートアップスクリプトでスクリプトファイル生成

--- startup.sh
+++ startup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-sleep 120s
+echo <<EOF > starup.sh
 eval $(ssh-agent)
 ssh-add /root/.ssh/id_rsa
 git clone https://github.com/mememeil/cloud-build-test.git /root/cloud-build-test
 inspec exec cloud-build-test/inspec/controls/test-server1.rb -t ssh://kodhikodhi@10.138.0.11 > /home/kodhikodhi/result 2>&1
+EOF
+
+. ./starup.sh
