f7c0c7f5b7b9fbdf7331cbbb499d0bf99db7189e
mememeil <kodhikodhi@gmail.com>
2020-09-27 16:52:24 +0900

    cloudbuild.yaml ssh-addの鍵名を修正

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 eval $(ssh-agent)
-ssh-add /root/.ssh/id-rsa
+ssh-add /root/.ssh/id_rsa
 git clone https://github.com/mememeil/cloud-build-test.git /root/cloud-build-test
 inspec exec cloud-build-test/inspec/controls/test-server1.rb -t ssh://kodhikodhi@10.138.0.11 > /home/kodhikodhi/result 2>&1
