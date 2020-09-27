05ddc05aedec7c10c4bbbe88b06fc2a7d843eaed
mememeil <kodhikodhi@gmail.com>
2020-09-27 16:18:35 +0900

    cloudbuild.yaml変更

--- startup.sh
+++ startup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+su - kodhikodhi <<EOF
 git clone https://github.com/mememeil/cloud-build-test.git cloud-build-test
+EOF
 eval $(ssh-agent)
 ssh-add /root/.ssh/id-rsa
 inspec exec cloud-build-test/inspec/controls/test-server1.rb -t ssh://kodhikodhi@10.138.0.10 > /home/kodhikodhi/result 2>&1
