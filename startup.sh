80a79301d293defb967e3c5d2f652c69b6931fee
mememeil <kodhikodhi@gmail.com>
2020-09-27 10:49:40 +0900

    スクリプトを改修

--- startup.sh
+++ startup.sh
@@ -2,5 +2,7 @@
 
 su - kodhikodhi <<EOF
 git clone https://github.com/mememeil/cloud-build-test.git cloud-build-test
+# eval $(ssh-agent)
+ssh-add
 inspec exec cloud-build-test/inspec/controls/test-server1.rb -t ssh://kodhikodhi@10.138.0.10 2> /home/kodhikodhi/result1
 EOF
